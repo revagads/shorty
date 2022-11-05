ed844142acccfc9a728cdf3cdb3c91d245eb69b9
revagads <117502367+revagads@users.noreply.github.com>
2022-11-05 12:35:17 +0700

    Create angka.php

---
+++ angka.php
@@ -0,0 +1,9 @@
+  //menangkap data angka yang di input
+            $satu=$_POST['satu'];
+            $dua=$_POST['dua'];
+            $tiga=$_POST['tiga'];
+            $empat=$_POST['empat'];
+            $lima=$_POST['lima'];
+            $enam=$_POST['enam'];
+            $tujuh=$_POST['tujuh'];
+            $delapan=$_POST['delapan'];
